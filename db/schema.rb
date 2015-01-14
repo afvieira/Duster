aeb6704317114d4f94134eaac93263a48beb0973
roger <rogerp062@outlook.com>
2015-01-14 15:25:26 +0000

    Removed most indexes from users

--- db/schema.rb
+++ db/schema.rb
@@ -226,8 +226,6 @@ ActiveRecord::Schema.define(version: 20141210155917) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["cc"], name: "index_users_on_cc", unique: true
-  add_index "users", ["cell_phone"], name: "index_users_on_cell_phone", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
