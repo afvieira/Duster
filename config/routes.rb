fb52d19abc0f12be77f23b77ebef284ac0c0b5cd
Jorge Oliveira <jorgemfo@gmail.com>
2015-01-14 15:37:52 +0000

    ficheiro adicionado request service

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get "users/new_user" => 'registrations#new_user'
       get "users/new_client" => 'registrations#new_client'
       post "users/create_client" => "registrations#create_client"
+      get "services/request_service" => 'services#request_service'
     end
     resources :guestbooks, :answer_types, :answers, :additional_informations, :states, :histories, :rankings, :services, :feedbacks, :payment_types, :premia, :service_provider_premia, :days, :slots, :schedules, :service_types, :service_type_service_providers, :service_providers, :addresses
     get '*path', to: redirect { |params, request| "/#{params[:locale]}" }
